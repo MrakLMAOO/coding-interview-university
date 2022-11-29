262b769374b32b34bac2433223d45dc80181acfc
Filip Juran <filipjuranoe@gmail.com>
2022-11-29 02:50:38 +0100

    Add C to F in for.c

--- C Book Vol2 (kopie)/for.c
+++ C Book Vol2 (kopie)/for.c
@@ -3,6 +3,9 @@
 int main()
 {
     int fahr;
+    int cel;
     for (fahr = 300; fahr >= 0; fahr = fahr - 20)
         printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    for (cel = 300; cel >= 0; cel = cel - 20)
+        printf("%3d %6.1f\n", cel, (1.8 * cel)+32);
 }
